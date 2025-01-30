631136d4237a6bcfb006bcd733c42a52c0b0cbc8
javiers78889 <javiers78889@gmail.com>
2025-01-30 09:11:38 -0500

    mejoras

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -19,5 +19,5 @@ export const ConfirmAccountSchema = z.string().min(6,{message:'Token no valido, 
 
 export const LoginSchema = z.object({
     email: z.string().min(1,{message:'El email es Obligatorio para iniciar sesion'}).email({message:'No es un formato válido para email'}),
-    password: z.string()
+    password: z.string().min(1,{message:'El Password no puede ir vacío'})
 })
