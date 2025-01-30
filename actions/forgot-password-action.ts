fad767a3c79f6080f455060278b173b6d8e429a7
javiers78889 <javiers78889@gmail.com>
2025-01-30 13:57:48 -0500

    restableciendo datos

--- actions/forgot-password-action.ts
+++ actions/forgot-password-action.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { ForgotSchema } from "@/src/schemas"
+import { cookies } from "next/headers"
 
 type ActionForgotPassword = {
     success: string,
@@ -11,10 +12,21 @@ export const forgotPassword = async (prevstate: ActionForgotPassword, formData: 
     const forgotData = {
         email : formData.get('email')
     }
-
+    const token = cookies().get('jwt')
     const RecoverData = ForgotSchema.safeParse(forgotData)
+    const email = RecoverData.data?.email
+    const url = `${process.env.API_URL}/auth/users/${email}`
 
-    console.log(RecoverData.data)
+    const req = await fetch(url, {
+        headers:{
+            
+            Authorization : `Bearer ${token}`
+        }
+    })
+
+    const json = req.json()
+
+    console.log(json)
 
     return {
         success: '',
