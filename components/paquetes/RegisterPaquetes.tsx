e78c35f2c3ed64e5cdecf258351ad41e9c178a17
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-02-07 11:10:48 -0500

    Update RegisterPaquetes.tsx

--- components/paquetes/RegisterPaquetes.tsx
+++ components/paquetes/RegisterPaquetes.tsx
@@ -19,6 +19,7 @@ export default function RegisterPaquetes() {
         }
         if(state.success){
             toast.success(state.success)
+            ref.current?.reset()
         }
         
     },[state])
