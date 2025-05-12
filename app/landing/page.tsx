78cc892c57bddd31d98c4992db51fbdc29cda8f1
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-05-12 14:58:50 -0500

    sin suspense

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -2,7 +2,7 @@ import NuestrosServicios from '@/components/landingpage/NuestrosServicios'
 import Principal from '@/components/landingpage/Principal'
 import RastrearPaquete from '@/components/landingpage/RastrearPaquete'
 import Tiendas from '@/components/landingpage/Tiendas'
-import React, { Suspense } from 'react'
+import React from 'react'
 
 export default function page() {
     return (
