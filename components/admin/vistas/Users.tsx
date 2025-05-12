2c908f11f234e356f1274137de52fbfcb59892b6
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-05-12 15:46:19 -0500

    correcciones

--- components/admin/vistas/Users.tsx
+++ components/admin/vistas/Users.tsx
@@ -15,8 +15,8 @@ export default function Users({ user }: { user: TUser }) {
             {user.correo?(''):(<Link href={'/admin/perfil/edit'} className='text-xl text-red-600 font-bold hover:underline text-wrap'>**¡Por favor agregue un correo para recibir su factura al correo aquí!**</Link>)}
             <div className='flex flex-col-reverse md:flex-row md:justify-between items-center shadow-md p-5 bg-white'>
                 <div className='w-full md:w-auto '>
-                    <h1 className="font-black text-4xl  my-5">Mis <span className='text-red-600'>Paquetes</span></h1>
-                    <p className="text-xl font-bold">Observa y Verifica tus {''}
+                    <h1 className="font-black text-4xl text-black my-5">Mis <span className='text-red-600'>Paquetes</span></h1>
+                    <p className="text-xl font-bold text-black">Observa y Verifica tus {''}
                         <span className="text-amber-500">Paquetes</span>
                     </p>
                 </div>
