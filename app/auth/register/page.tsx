8436861d4390ace26bcfb3e7d336cc1d6a207e3d
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-05-12 13:06:51 -0500

    ajustados

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -10,7 +10,7 @@ export default function page() {
     return (
         <>
             <h1 className="font-black text-6xl text-red-600">Crear una cuenta</h1>
-            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
+            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">Compras Por Internet</span></p>
             <RegisterForm />
             <nav className="mt-10 flex flex-col space-y-4">
 
