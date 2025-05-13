25c49535fe8e338a1191e207e35b1352e95a9f2c
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-05-12 19:49:11 -0500

    ups

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 "use client"
 
-import { rastreoPaquetes } from "@/actions/rastreo-paquetes-action";
-import ToasterNotification from "@/components/ui/ToasterNotification";
-import Image from "next/image";
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import { useEffect, useRef } from "react";
-import { useFormState } from "react-dom";
-import { toast } from "react-toastify";
+
 
 
 
