9abe271ef9edeeb22c280112d47507cdb801df8a
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-05-12 20:13:21 -0500

    whatsapp

--- app/landing/layout.tsx
+++ app/landing/layout.tsx
@@ -22,7 +22,7 @@ export default function LayoutL({
       <main className="flex-1 min-h-screen">
         {children}
       </main>
-      <a href="https://res.cloudinary.com/dga0uaurm/image/upload/v1747097864/_Pngtree_whatsapp_social_media_icon_design_3654780-removebg-preview_olpu6g.png" className="fixed bottom-4 right-4 z-50 cursor-pointer rounded-full p-3 bg-green-500">
+      <a  href="https://wa.me/50765474870" className="fixed bottom-4 right-4 z-50 cursor-pointer rounded-full p-3 bg-green-500">
         <MessageCircle height={50} width={50} />
       </a>
       <FooterL />
