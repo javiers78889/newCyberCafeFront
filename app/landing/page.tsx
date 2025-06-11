1c43e92d317b80cf1dcd01670d935e080e6a1512
javiers78889 <javiers78889@gmail.com>
2025-06-11 02:27:27 -0500

    mamut

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -3,6 +3,7 @@ import Principal from '@/components/landingpage/Principal'
 import RastrearPaquete from '@/components/landingpage/RastrearPaquete'
 import Tiendas from '@/components/landingpage/Tiendas'
 import React from 'react'
+/*asdasdasd*/ 
 
 export default function page() {
     return (
