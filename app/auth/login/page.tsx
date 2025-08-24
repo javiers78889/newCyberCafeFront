f1cb192e4ba90d98dc8fb14788a93c8adef8eb25
javiers78889 <javiers78889@gmail.com>
2025-08-24 14:36:00 -0500

    nuevo

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,10 +3,10 @@ import LoginForm from "@/components/auth/LoginForm"
 import type { Metadata } from 'next'
 import Link from "next/link"
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "AshBranding&Import - INICIAR SESION",
   description: "INICIAR SERSION",
-   icons: {
+  icons: {
     icon: [
       { url: '/fete.png', sizes: '32x32', type: 'image/png' },
       { url: '/fete.png', sizes: '16x16', type: 'image/png' }
@@ -18,18 +18,18 @@ export const metadata = {
 export default function page() {
 
 
-    return (
-        <>
-            <h1 className="font-black text-6xl text-red-500">Inicia Sesión</h1>
-            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">Compras Por Internet</span></p>
-            <LoginForm />
-            <nav className="mt-10 flex flex-col space-y-4">
+  return (
+    <>
+      <h1 className="font-black text-6xl text-red-500">Inicia Sesión</h1>
+      <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">Compras Por Internet</span></p>
+      <LoginForm />
+      <nav className="mt-10 flex flex-col space-y-4">
 
-                <Link href='/auth/register' className="text-center text-gra">¿No tienes cuenta?. Registrate</Link>
-                <Link href='/auth/forgot-password' className="text-center text-gra">¿Olvidaste tu contraseña?</Link>
+        <Link href='/auth/register' className="text-center text-gra">¿No tienes cuenta?. Registrate</Link>
+        <Link href='/auth/forgot-password' className="text-center text-gra">¿Olvidaste tu contraseña?</Link>
 
 
-            </nav>
-        </>
-    )
+      </nav>
+    </>
+  )
 }
